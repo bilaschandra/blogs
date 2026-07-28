8ce54cfce2e00c69588e7913e7db3bee31262b77
bilaschandra <bilas.chandra@fieldnation.com>
2026-07-28 13:52:00 +0600

    Add cached MongoDB connection module

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,27 @@
+import { MongoClient, type Db } from "mongodb";
+
+const uri = process.env.DATABASE_URL;
+if (!uri) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    global._mongoClientPromise = new MongoClient(uri).connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  clientPromise = new MongoClient(uri).connect();
+}
+
+export async function getDb(): Promise<Db> {
+  const client = await clientPromise;
+  return client.db();
+}
